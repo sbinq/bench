b412f2033106b7dc28ff5b33db6bf6c51e63da9f
Chirayu Patel <chirayu.patel@flipkart.com>
2015-12-17 16:55:37 +0530

    removing token stream out. I think it can be evolved to support spikes

---
+++ tokens.go
@@ -0,0 +1,43 @@
+package bench
+
+import "time"
+import "sync"
+
+// TokenStream generates a stream of tokens
+type TokenStream struct {
+	rps int
+	S chan bool
+	closeCh chan bool
+	wg *sync.WaitGroup
+} 
+
+// Stop stops the token stream
+func (t *TokenStream) Stop() {
+	close(t.closeCh)
+	close(t.S)
+}
+
+// TODO: generation is at a constant rate and doesn't accomodate spikes.
+func (t *TokenStream) generate() {
+	go func() {
+		ticker := time.NewTicker(time.Second / time.Duration(t.rps))
+		defer ticker.Stop()
+		for {
+			select {
+			case <-t.closeCh:
+				return
+			case <-ticker.C:
+				t.S <- true
+			}
+		}
+	}()
+}
+
+// NewTokenStream creates a new token stream
+func NewTokenStream(rps int) * TokenStream{
+	t := TokenStream{rps:rps}
+	t.closeCh = make(chan bool)
+	t.S = make(chan bool, 10*rps)
+	t.generate()
+	return &t
+}
