bf295ff71b0c2263e11b9b7d3270216a7adfed41
Chirayu Patel <chirayu.patel@flipkart.com>
2015-12-17 11:12:21 +0530

    added uniform run

--- examples/simple.go
+++ examples/simple.go
@@ -9,11 +9,23 @@ import (
 
 func f(c *bench.Context) {
 	time.Sleep(100 * time.Millisecond)
-	c.RecordTime(time.Duration(c.Iteration) * time.Millisecond)
+	c.Timer("Latency", time.Duration(c.Iteration) * time.Millisecond)
+	c.Incr("Call Counter", 1)
+}
+
+func f1(c *bench.Context) {
+	c.Timer("Latency", time.Duration(c.Iteration) * time.Millisecond)
+	c.Incr("Call Counter", 1)
 }
 
 func main() {
-	b := bench.NewBench(10, time.Second*2, f)
-	b.Run()
-	fmt.Printf("%s", b)
+	// run at full steam
+	// b := bench.NewBench(1, time.Second*2, f)
+	// b.Run()
+	// fmt.Printf("%s", b)
+	
+	// run at the provided rps
+	b1 := bench.NewBench(2, time.Second*5, f)
+	b1.UniformRun(10)
+	fmt.Printf("%s", b1)
 }
--- examples/simplehttp.go
+++ examples/simplehttp.go
@@ -8,17 +8,30 @@ import (
 )
 
 func f(c *bench.Context) {
+	var client *http.Client 
+	v, ok := c.Values["client"]
+	if !ok {
+		client = &http.Client{}
+		c.Incr("Counter", 1)
+		c.Values["client"] = client
+	} else {
+		client = v.(*http.Client)
+	}
+	
 	start := time.Now()
-	resp, err := http.Get("http://www.flipkart.com")
+	resp, err := client.Get("http://localhost:8000")
 	if err != nil {
-		fmt.Printf("Error %s", err)
+		fmt.Printf("Error :  %s\n", err)
+		//c.Incr("Errors", 1)
+		return
 	}
 	defer resp.Body.Close()
-	c.RecordTime(time.Since(start))
+	c.Timer("Latency", time.Since(start))
+	// c.Incr("Counter", 1)
 }
 
 func main() {
 	b := bench.NewBench(10, time.Second*10, f)
-	b.Run()
+	b.UniformRun(100)
 	fmt.Printf("%s", b)
 }
